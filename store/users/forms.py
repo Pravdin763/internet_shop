5b70c43e9deb2c4de38339a8d55175c3d380f5d7
Pravdin763 <igor.pravdin163@gmail.com>
2023-06-23 19:25:49 +0400

    7 урок. авторизация и аутентификация

---
+++ store/users/forms.py
@@ -0,0 +1,40 @@
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django import forms
+from users.models import User
+
+class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={
+
+    'class' : "form-control py-4", 'placeholder': "Введите имя пользователя",
+    }))
+
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': "form-control py-4", 'placeholder': "Введите пароль"
+    }))
+    class Meta:
+        model = User
+        fields = ('username', 'password')
+
+class UserRegisterationForm(UserCreationForm):
+    first_name = forms.CharField(widget=forms.TextInput(attrs={
+    'class' : "form-control py-4", 'placeholder': "Введите имя"
+    }))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={
+    'class' : "form-control py-4", 'placeholder': "Введите фамилию"
+    }))
+    username = forms.CharField(widget=forms.TextInput(attrs={
+    'class' : "form-control py-4", 'placeholder': "Введите имя пользователя"
+    }))
+    email = forms.CharField(widget=forms.EmailInput(attrs={
+    'class' : "form-control py-4", 'placeholder': "Введите адрес эл. почты"
+    }))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
+    'class' : "form-control py-4", 'placeholder': "Введите пароль"
+    }))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+    'class': "form-control py-4", 'placeholder': "Подтвердите пароль"
+    }))
+
+    class Meta:
+        model = User
+        fields = ("first_name", "last_name", "username", "email", "password1", "password2")
--- store/users/views.py
+++ store/users/views.py
@@ -1,7 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib import auth
+
+from users.models import User
+from users.forms import UserLoginForm, UserRegisterationForm
+
 
 def login(request):
-    return render(request, 'users/login.html')
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect('/')
+    else:
+        form = UserLoginForm()
+    context = {'form': form}
+    return render(request, 'users/login.html', context)
 
 def registration(request):
-    return render(request, 'users/registration.html')
+    if request.method=='POST':
+        form = UserRegisterationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/')
+    else:
+        form= UserRegisterationForm()
+    context = {'form': form}
+    return render(request, 'users/registration.html', context)
